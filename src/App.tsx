46d507013eff389089368ad6695a3a698f1d4f64
James Karlsson <jameskarlsson1@gmail.com>
2024-07-28 23:16:59 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { createSignal } from "solid-js";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  return <div>Test</div>;
+}
+
+export default App;
