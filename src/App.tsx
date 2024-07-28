7f33d213d55b9defd9060734e7e71d478638d402
James Karlsson <jameskarlsson1@gmail.com>
2024-07-28 23:41:45 +1000

    FFMPEG stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,72 @@
+import { createSignal } from "solid-js";
 import "./App.css";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile, toBlobURL } from "@ffmpeg/util";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <FFM />
+    </div>
+  );
+}
+
+function FFM() {
+  const [loaded, setLoaded] = createSignal(false);
+  let ffmpegRef = new FFmpeg();
+  let videoRef: HTMLVideoElement | null = null;
+  let messageRef: HTMLParagraphElement | null = null;
+
+  const load = async () => {
+    const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
+    const ffmpeg = ffmpegRef;
+    ffmpeg.on("log", ({ message }) => {
+      if (!messageRef) throw new Error("Expected message div");
+      messageRef.innerHTML = message;
+      console.log(message);
+    });
+    // toBlobURL is used to bypass CORS issue, urls with the same
+    // domain can be used directly.
+    await ffmpeg
+      .load({
+        coreURL: await toBlobURL(
+          `${baseURL}/ffmpeg-core.js`,
+          "text/javascript",
+        ),
+        wasmURL: await toBlobURL(
+          `${baseURL}/ffmpeg-core.wasm`,
+          "application/wasm",
+        ),
+      })
+      .catch((e) => console.error(e));
+    setLoaded(true);
+  };
+
+  const transcode = async () => {
+    const ffmpeg = ffmpegRef;
+    await ffmpeg.writeFile(
+      "input.webm",
+      await fetchFile(
+        "https://raw.githubusercontent.com/ffmpegwasm/testdata/master/Big_Buck_Bunny_180_10s.webm",
+      ),
+    );
+    await ffmpeg.exec(["-i", "input.webm", "output.mp4"]);
+    const data = await ffmpeg.readFile("output.mp4");
+    if (!videoRef) return;
+    videoRef.src = URL.createObjectURL(new Blob([data], { type: "video/mp4" }));
+  };
+
+  return loaded() ? (
+    <>
+      <video ref={(el) => (videoRef = el)} controls></video>
+      <br />
+      <button onClick={transcode}>Transcode webm to mp4</button>
+      <p ref={(el) => (messageRef = el)}></p>
+      <p>Open Developer Tools (Ctrl+Shift+I) to View Logs</p>
+    </>
+  ) : (
+    <button onClick={load}>Load ffmpeg-core (~31 MB)</button>
+  );
 }
 
 export default App;
